a6ffb2e8a59efccbdb2d1ca557eabc581de73013
qinxiaohui [秦晓辉] <qinxiaohui@xiaomi.com>
2014-03-25 13:45:19 +0800

    fixbug

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"bufio"
-	ws "code.google.com/p/go.net/websocket"
+	"code.google.com/p/go.net/websocket"
 	"fmt"
 	"github.com/astaxie/beego/config"
 	"github.com/ulricqin/goutils/filetool"
@@ -18,7 +18,7 @@ var cfgFile = "/etc/guacamole/guacamole.properties"
 var Config config.ConfigContainer
 var BaseLogDir string
 
-func ReadLog(ws *ws.Conn) {
+func ReadLog(ws *websocket.Conn) {
 	var err error
 	var dir string
 	var file string
@@ -27,20 +27,20 @@ func ReadLog(ws *ws.Conn) {
 
 		var msg string
 
-		if err = ws.Message.Receive(ws, &msg); err != nil {
+		if err = websocket.Message.Receive(ws, &msg); err != nil {
 			fmt.Println("Can't receive. Error:", err.Error())
 			break
 		}
 
 		if msg == "" {
-			ws.Message.Send(ws, "msg is blank")
+			websocket.Message.Send(ws, "msg is blank")
 			break
 		}
 
 		msg = strings.Trim(msg, " ")
 		idx := strings.Index(msg, ";")
 		if idx < 0 {
-			ws.Message.Send(ws, "msg has no ';' ")
+			websocket.Message.Send(ws, "msg has no ';' ")
 			break
 		}
 
@@ -51,40 +51,40 @@ func ReadLog(ws *ws.Conn) {
 
 		dir = strings.Split(dirStr, ":")[1]
 		if dir == "" {
-			ws.Message.Send(ws, "dir is blank")
+			websocket.Message.Send(ws, "dir is blank")
 			break
 		}
 
 		file = strings.Split(fileStr, ":")[1]
 		if file == "" {
-			ws.Message.Send(ws, "file is blank")
+			websocket.Message.Send(ws, "file is blank")
 			break
 		}
 
 		logPath := path.Join(BaseLogDir, dir, file)
 		if !filetool.IsExist(logPath) {
-			ws.Message.Send(ws, fmt.Sprintf("file: %s not exists", path.Join(dir, file)))
+			websocket.Message.Send(ws, fmt.Sprintf("file: %s not exists", path.Join(dir, file)))
 			break
 		}
 
-		var file *File
+		var fi *os.File
 
-		file, err := os.Open(logPath)
+		fi, err := os.Open(logPath)
 		if err != nil {
-			ws.Message.Send(ws, fmt.Sprintf("file: %s cannot open", path.Join(dir, file)))
+			websocket.Message.Send(ws, fmt.Sprintf("file: %s cannot open", path.Join(dir, file)))
 			break
 		}
 
-		defer file.Close()
+		defer fi.Close()
 
-		reader := bufio.NewReader(file)
+		reader := bufio.NewReader(fi)
 		var line []byte
 		for {
-			line, err = reader.ReadLine()
+			line, _, err = reader.ReadLine()
 			if err == os.EOF {
 				break
 			} else {
-				if err = ws.Message.Send(ws, string(line)); err != nil {
+				if err = websocket.Message.Send(ws, string(line)); err != nil {
 					fmt.Println("Can't send. Msg:", err.Error())
 					break
 				}
@@ -110,7 +110,7 @@ func main() {
 	}
 
 	http.Handle("/", http.FileServer(http.Dir("."))) // <-- note this line
-	http.Handle("/ws", ws.Handler(ReadLog))
+	http.Handle("/ws", websocket.Handler(ReadLog))
 
 	if err := http.ListenAndServe(":8123", nil); err != nil {
 		log.Fatal("ListenAndServe :8123 Error. Msg:", err)
